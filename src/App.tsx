b2d7e3a5620a7e22299b95103c5cbe498d479db7
Kory <korymbecker@gmail.com>
2022-02-08 10:25:51 -0500

    updated App

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,42 @@
-import React from "react";
+import { useState, useEffect, FC } from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import axios from "axios";
+import Home from "./pages/Home";
+import Chats from "./pages/Chats";
+import IChat from "./interfaces/IChat";
+import Navbar from "./components/Navbar";
 
-function App() {
-  return <div className="App">poop</div>;
-}
+const App: FC = (): JSX.Element => {
+  const [chats, setChats] = useState<IChat[]>([]);
+
+  useEffect(() => {
+    let mounted = true;
+    axios
+      .get<IChat[]>("http://localhost:3001/api/v1/chat")
+      .then((response) => {
+        if (mounted) {
+          setChats(response.data);
+        }
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+    return () => {
+      mounted = false;
+    };
+  }, []);
+
+  return (
+    <Router>
+      <div className="App">
+        <Navbar />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/chat" element={<Chats />} />
+        </Routes>
+      </div>
+    </Router>
+  );
+};
 
 export default App;
